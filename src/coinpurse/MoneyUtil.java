57275d0db8431c292d2ccc85341b06348db09cc8
Septima777 <noppawan.kul@ku.th>
2018-01-19 17:53:04 +0700

    add MoneyUntil

--- src/coinpurse/MoneyUtil.java
+++ src/coinpurse/MoneyUtil.java
@@ -8,11 +8,17 @@ import java.util.Collections;
 public class MoneyUtil {
 
 	public static void sortCoins(List<Coin> coins) {
-		
+		Collections.sort(coins);
 	}
 
 	public static List<Coin> filterByCurrency(List<Coin> coins, String currency) {
-
+		List<Coin> sameCurrency = new ArrayList<>();
+		for(Coin coin : coins){
+			if(coin.getCurrency().contains(currency)){
+				sameCurrency.add(coin);
+			}
+		}
+		return sameCurrency;
 	}
 
 }
